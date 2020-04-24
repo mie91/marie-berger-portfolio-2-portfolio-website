dbe8247e7aa9fa6f492a47dde21027fab500ae32
Marie Helen Berger <44783768+mie91@users.noreply.github.com>
2020-04-24 19:49:42 +0200

    Fixed portal

--- src/components/portfolio/webDesign/WebDesign.js
+++ src/components/portfolio/webDesign/WebDesign.js
@@ -1,36 +1,56 @@
 import React from 'react';
-import { Container, Col, Row, Image, Card, Accordion } from "react-bootstrap";
+import { Container, Col, Row, Image, Card, Button } from "react-bootstrap";
 import MainHeader from "../../layout/MainHeader";
 import SubHeader from "../../layout/SubHeader";
 import BackToTop from "../../layout/BackToTop";
-import PlaceHolder from "../../../assets/images/spaceX_1.jpg";
+import SpaceX from "../../../assets/images/spaceX_1.jpg";
+import Recycle from "../../../assets/images/recycle_1.jpg";
+import Gameof from "../../../assets/images/gameof_1.jpg";
 
 function WebDesign () {
 return (
 <>
     <div className="main-container dark">
         <Container>
-            <div className="content-box content-box-portfolio-portal">
+            <div className="content-box content-box-portfolio-portal ">
                 <MainHeader title="webdesign" />
-                    <SubHeader title="School Projects" />
-                    <Row>         
-                    <Col className="col-md-12">
-                            <div className="project-thumbnails-container  justify-content-center">
-                                
-                                <a href="/"><img className="btnIcon" src={PlaceHolder} alt="Instagram Logo" /></a>
-                                <a href="/"><img className="btnIcon" src={PlaceHolder} alt="Facebook Logo" /></a>
-                                <a href="/"><img className="btnIcon" src={PlaceHolder} alt="Git Hub logo" /></a>
-                            </div>
-                            </Col>
-                        </Row>
-                    </div>
-                    <BackToTop/>
-                 </Container>
+                <SubHeader title="School Projects" />
+                    <Row className="justify-content-center">
+
+                        <Card className="portfolio-portal-card">
+                            <a href="/"><Card.Img variant="top" src={Recycle} alt="screenshot from project" />
+                                <Card.Body>
+                                   <SubHeader title="Re-Cycle"/>
+                                   
+                                </Card.Body></a>
+                            </Card>
+                        
+                        <Card className="portfolio-portal-card">
+                            <a href="/"><Card.Img variant="top" src={SpaceX} alt="screenshot from project" />
+                                <Card.Body>
+                                <SubHeader title="SpaceX" />
+                                    
+                                </Card.Body></a>
+                            </Card>
+                        
+
+                        
+                            <Card className="portfolio-portal-card">
+                            <a href="/"><Card.Img variant="top" src={Gameof} alt="screenshot from project" />
+                                <Card.Body>
+                                <SubHeader title="Game Of Tubbies" />
+                                    
+                                </Card.Body></a>
+                            </Card>
+                        
+
+                   
+                </Row>
             </div>
-        </>
-    );
+            <BackToTop />
+        </Container>
+    </div>
+</>
+);
 }
 export default WebDesign;
-
-
-
