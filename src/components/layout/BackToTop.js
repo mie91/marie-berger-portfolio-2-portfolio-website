02272d0f03dc9a6c36a8f5ae51cd4c8ba0217b7f
Marie Helen Berger <44783768+mie91@users.noreply.github.com>
2020-04-26 12:05:09 +0200

    added layout folder

---
+++ src/components/layout/BackToTop.js
@@ -0,0 +1,14 @@
+import React from "react";
+import IconUp from "../../assets/icons/icon_up.png";
+
+const BackToTop = function () {
+    function ScrollToTop() {
+        window.scrollTo({ behavior: "smooth", top: 0 });
+    }
+
+    return  <div className="to-top-container">
+                <button className="return-top-btn" onClick={ScrollToTop}><img className="btnIcon" src={IconUp} alt="Up arrow" /></button>
+            </div>;
+};
+
+export default BackToTop;
